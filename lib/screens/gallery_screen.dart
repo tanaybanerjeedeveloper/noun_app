e1cd8a8ad4b54094f9207118c29275b735fae7da
tanaybanerjeedeveloper <banerjeetanay333@gmail.com>
2022-05-05 15:42:53 +0530

    added gallery ui

--- lib/screens/gallery_screen.dart
+++ lib/screens/gallery_screen.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 
 import '../utilities/constants.dart';
+import '../widgets/carousel_widget.dart';
 
 class GalleryScreen extends StatelessWidget {
   //const GalleryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      backgroundColor: kScaffoldBackgroundColor,
-      body: Center(
-        child: Text(
-          'Gallery Screen',
-          textAlign: TextAlign.center,
+        backgroundColor: kScaffoldBackgroundColor,
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: kScaffoldBackgroundColor,
+          title: Text('Gallery'),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            CarouselWidget(
+                mediaQuery: mediaQuery, text: 'Video', isVideo: true),
+            SizedBox(
+              height: mediaQuery.size.height * 0.1,
+            ),
+            CarouselWidget(
+                mediaQuery: mediaQuery, text: 'Images', isVideo: true),
+          ],
+        ));
   }
 }
