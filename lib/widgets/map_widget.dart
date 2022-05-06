6cf7d08a4a0a98811b446a66186badc411bfcc57
tanaybanerjeedeveloper <banerjeetanay333@gmail.com>
2022-05-06 16:16:50 +0530

    style: updated home ui

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -3,6 +3,8 @@ import 'package:mapmyindia_gl/mapmyindia_gl.dart';
 import 'package:location/location.dart';
 import 'package:flutter/foundation.dart';
 
+import '../utilities/constants.dart';
+
 class MapWidget extends StatefulWidget {
   //MyFlexibleAppBar({Key? key}) : super(key: key);
 
@@ -50,19 +52,46 @@ class _MapWidgetState extends State<MapWidget> {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
-    return Container(
-      height: mediaQuery.height * 0.6,
-      child: MapmyIndiaMap(
-        initialCameraPosition: _kInitialPosition,
-        onMapCreated: (map) {
-          mapController = map;
-          mapController.getMapmyIndiaStyle();
-        },
-        onMapClick: (point, latlng) => {
-          print(latlng.toString()),
-          // Fluttertoast.showToast(msg: latlng.toString(), toastLength: Toast.LENGTH_LONG, gravity: ToastGravity.BOTTOM)
-        },
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: mediaQuery.height * 0.6,
+          child: MapmyIndiaMap(
+            initialCameraPosition: _kInitialPosition,
+            onMapCreated: (map) {
+              mapController = map;
+              mapController.getMapmyIndiaStyle();
+            },
+            onMapClick: (point, latlng) => {
+              print(latlng.toString()),
+              // Fluttertoast.showToast(msg: latlng.toString(), toastLength: Toast.LENGTH_LONG, gravity: ToastGravity.BOTTOM)
+            },
+          ),
+        ),
+        Positioned(
+          //top: 2,
+          child: Padding(
+            padding: EdgeInsets.only(right: 55.0, left: 55.0, top: 30.0),
+            child: TextField(
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                hintText: 'Destination',
+                prefixIcon: Icon(
+                  Icons.location_on,
+                  color: Colors.white,
+                ),
+                filled: true,
+                fillColor: Theme.of(context).colorScheme.secondary,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
